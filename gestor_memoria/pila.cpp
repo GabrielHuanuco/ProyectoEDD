1240014e31677c2a133df170ea9221c3a5cf47d9
GabrielHuanuco <167567071+GabrielHuanuco@users.noreply.github.com>
2025-05-27 23:23:33 -0500

    Gabriel Huanuco
    Desarrollo de la funcion "Asignar Memoria"

--- gestor_memoria/pila.cpp
+++ gestor_memoria/pila.cpp
@@ -7,3 +7,13 @@ struct BloqueMemoria //Estructura para representar el bloque de memoria
     int tamanio; //Valor asignado al tamaño del proceso
     BloqueMemoria *siguiente; //Puntero al siguiente bloque en la pila
 };
+
+void asignarMemoria(BloqueMemoria *&cima, int ID_Proceso, int tamanio) //Funcion para asignar un bloque de memoria
+{
+    BloqueMemoria *nuevo = new BloqueMemoria; //Se crea un nuevo bloque de memoria
+    nuevo->ID_Proceso = ID_Proceso; //Se asigna los nuevos valores
+    nuevo->tamanio = tamanio; //Se asigna un nuevo tamaño
+    nuevo->siguiente = cima; //El nuevo bloque apunta al que antes era la cima
+    cima = nuevo; //Ahora el nuevo bloque es la cima
+    cout<<"Memoria asignada correctamente"; //Mensaje que afirma que el proceso fue agregado correctamente
+}
