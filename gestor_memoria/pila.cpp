5c65f5e7b8d71ab7cf07df2751b58e5885ca1f84
GabrielHuanuco <167567071+GabrielHuanuco@users.noreply.github.com>
2025-05-27 22:27:52 -0500

    Gabriel Huanuco
    Ga PE

--- gestor_memoria/pila.cpp
+++ gestor_memoria/pila.cpp
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-int main(){
-    cout<<"Holi";
-=======
 #include <iostream>
 using namespace std;
+
 int main()
 {
-    cout << "hola mundo";
-    return 0;
->>>>>>> 676c36e50e0767f390549f8813a486e75241e712
+    cout<<"GA";
 }
