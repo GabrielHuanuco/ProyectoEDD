f9fed292ad40a40d1b4954673b580c42bb88f6f4
Cesar1606 <167567227+Cesar1606@users.noreply.github.com>
2025-05-28 19:00:16 -0500

    Update lista_enlazada.cpp

--- gestor_procesos/lista_enlazada.cpp
+++ gestor_procesos/lista_enlazada.cpp
@@ -1,13 +1,1 @@
-#include <iostream> // Incluye la librería estándar de entrada/salida
-using namespace std;
 
-// -------------------- GESTOR DE PROCESOS (LISTA ENLAZADA) --------------------
-
-// Estructura para representar un proceso en la lista enlazada
-struct Proceso
-{
-    int id;             // Identificador único del proceso
-    char nombre[30];    // Nombre del proceso (arreglo de caracteres)
-    int prioridad;      // Prioridad del proceso
-    Proceso *siguiente; // Puntero al siguiente proceso en la lista
-};
